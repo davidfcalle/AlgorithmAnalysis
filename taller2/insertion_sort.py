4b7dbfb791fd32cffd7e633a7dc6805a1592e985
David Calle <davidcalle9430@gmail.com>
2016-02-09 21:24:27 -0500

    Fixed insertion sort bug

--- taller2/insertion_sort.py
+++ taller2/insertion_sort.py
@@ -2,7 +2,7 @@ def insertion_sort(S):
     for j in range ( 1 , len(S) ):
         k = S[j]
         i = j - 1
-        while( i > 0 and S[i] > k):
+        while( i >= 0 and S[i] > k):
             S[ i + 1 ] = S[ i ]
             i = i - 1
         S[ i + 1 ] = k
