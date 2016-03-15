3dd21563e8baa2598568a79a75b59ff2631406af
David Calle <davidcalle9430@gmail.com>
2016-03-15 17:45:00 -0500

    Fixed insertion error

--- quiz3/lcs.py
+++ quiz3/lcs.py
@@ -1,7 +1,6 @@
 
 
 def lcs_recursive( S1, i1 , S2 , i2 ):
-        print "i1: %i i2:%i" %( i1, i2  )
         if i1 < 0 or i2 < 0:
             return 0
         elif  S1[ i1 ] == S2[ i2 ]:
@@ -12,12 +11,11 @@ def lcs_recursive( S1, i1 , S2 , i2 ):
 def lcs_memoized_helper( S1 , S2 ):
     matrix = [ None ] * len( S1 )
     for l in range( len( S1 )  ):
-        matrix[ l ] = [ 0 ] * len( S2 )
+        matrix[ l ] = [ None ] * len( S2 )
     print lcs_memoized( S1 , len(S1) -1 , S2 , len( S2 ) -1 , matrix)
     print matrix
 
 def lcs_memoized( S1 , i1 , S2 , i2 , matrix ):
-    print "i1: %i i2:%i" %( i1, i2  )
     if i1 < 0 or i2 < 0:
         return 0
     elif  S1[ i1 ] == S2[ i2 ]:
@@ -31,40 +29,65 @@ def lcs_memoized( S1 , i1 , S2 , i2 , matrix ):
 
 def lcs_memoized_iterative(  S1 , S2 ):
     """inicialiczacion de matriz"""
-    matrix = [ None ] * len( S1 )
+    matrix = [ 0 ] * len( S1 )
     for l in range( len( S1 )  ):
         matrix[ l ] = [ 0 ] * len( S2 )
     """ metodo """
     i1 = len( S1 )
     i2 = len( S2 )
     max_l = 0
-    z = []
     for i in range( i1 ):
         for j in range( i2 ):
             if S1[ i ] == S2[ j ]:
-                #z.append( S1[i])
                 if i == 0 or j == 0:
                     matrix[ i ][ j ] = 1
                 else:
-                    matrix[ i ][ j ] = matrix[i-1][j-1] + 1
-
+                    matrix[ i ][ j ] = 1 + matrix[ i - 1 ][ j - 1 ]
             else:
-                max_previous = max(matrix[i][j-1], matrix[i-1][j])
-                matrix[ i ][ j ] = max_previous
-
-    print matrix
-    print z
+                if i == 0 or j == 0:
+                    matrix[ i ][ j ] = 0
+                else:
+                    matrix[ i ][ j ] = max( matrix[ i ][ j - 1 ] , matrix[ i - 1 ][ j ] )
+    """ obtencion de configuracion """
+    result = []
+    finished = False
+    i = len( S1 ) - 1
+    j = len( S2 ) - 1
+    while finished == False :
+        if i == 0 or j == 0:
+            if S1[ i ] == S2[ j ]:
+                result.insert( 0 , S1[ i ] )
+                finished = True
+            else:
+                if i == j:
+                    finished = True
+                else:
+                    if i > j:
+                        i = i - 1
+                    else:
+                        j = j - 1
+        else:
+            if S1[ i ] == S2[ j ]:
+                result.insert( 0 , S1[ i ] )
+                i = i - 1
+                j = j - 1
+            else:
+                if matrix[ i - 1 ][ j ] < matrix[ i ][ j - 1 ]:
+                    j = j - 1
+                else:
+                    i = i -1
+    return result
 
 
 def main():
-    S1 =    [ "A", "B", "C", "B", "D", "A", "B" ]
-    S2 = [ "B", "D", "C", "A", "B", "A" ]
+    #S1 =    [ "A", "B", "C", "B", "D", "A", "B" ]
+    #S2 = [ "B", "D", "C", "A", "B", "A" ]
     #S1 =    [ "A", "B"]
+    S1 = [ 1, 0, 0, 1, 0, 1, 0, 1 ]
     #S2 = [ "B", "A" ]
-    #S1 =    [ 1, 0, 0, 1, 0, 1, 0, 1 ]
-    #S2 = [ 0, 1, 0, 1, 1, 0, 1, 1, 0 ]
+    S2 = [ 0, 1, 0, 1, 1, 0, 1, 1, 0 ]
     #print lcs_recursive(  S1 , len(S1) -1 , S2 , len( S2 ) - 1 )
-    lcs_memoized_iterative( S1 , S2 )
+    print lcs_memoized_iterative( S1 , S2 )
 
 if __name__ == '__main__':
     main()
